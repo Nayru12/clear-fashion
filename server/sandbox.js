e3f85753e4c7a67bf0e8b3762fd02da8953fd3ec
Nayru12 <ip@inesperez.net>
2023-02-20 17:52:38 +0100

    feat(shop): json file

--- server/sandbox.js
+++ server/sandbox.js
@@ -1,20 +1,28 @@
 /* eslint-disable no-console, no-process-exit */
-const dedicatedbrand = require('./eshops/dedicatedbrand');
+
+/*const dedicatedbrand = require('./eshops/dedicatedbrand');
 const montlimartbrand = require('./eshops/montlimart');
-const circlesportswearbrand = require('./eshops/circlesportswear');
+const circlesportswearbrand = require('./eshops/circlesportswear');*/
 
 //async function sandbox (eshop = 'https://shop.circlesportswear.com/collections/t-shirts-homme') {
-async function sandbox (eshop, brand_rq) {
   //https://www.montlimart.com/99-vetements
   //https://www.dedicatedbrand.com/en/men/news'
   //https://shop.circlesportswear.com/collections/t-shirts-homme
 
-  
+
+async function sandbox (eshop, brand_rq) {
   try {
     console.log(`======== 🕵️‍♀️  browsing ${eshop} eshop ========`);
 
     const brand = require('./eshops/' + brand_rq);
     const products = await brand.scrape(eshop);
+
+    const json_products = products.map(product => JSON.stringify(product)).join('\n');
+
+    var fs = require('fs');
+    fs.appendFile("products.json", json_products, function(err, result) {
+      if(err) console.log('error', err);
+    });
 
     console.log(products);
     console.log('done');
@@ -32,23 +40,19 @@ const eshop = ['https://www.dedicatedbrand.com/en/men/all-men',
     'https://shop.circlesportswear.com/collections/collection-homme'];
 
 const brands = ['dedicatedbrand',
-      'montlimart',
-      'circlesportswear'];
+    'montlimart',
+    'circlesportswear'];
 
 
 async function display() {
   for (let i = 0; i < eshop.length; i++) {
     await sandbox(eshop[i], brands[i]);
   }
-  console.log('All done!');
-}
-
-//display();
-const result_products = sandbox(eshop[2], brands[2]);
-
-const json_file= JSON.stringify(result_products);
-
-//console.log(json_file);
+};
 
 var fs = require('fs');
-fs.writeFile("test.json", json_file);
+    fs.writeFile("products.json", '', function(err, result) {
+      if(err) console.log('error', err);
+    });
+
+display();
