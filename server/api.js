a4b580b0164ed4abc7499035ac716ae6673ba700
Nayru12 <ip@inesperez.net>
2023-03-21 16:40:45 +0100

    feat(final): v2.2

--- server/api.js
+++ server/api.js
@@ -84,3 +84,13 @@ app.get('/brands', async (request, response) => {
   const products = await collection.distinct('brand');
   response.send(products);
 });
+
+app.get('/products', async (request, response) => {
+  const client = await MongoClient.connect(MONGODB_URI, {'useNewUrlParser': true});
+  const db = client.db(MONGODB_DB_NAME);
+
+  const collection = db.collection('products');
+
+  const products = await collection.find({}).toArray();
+  response.send(products);
+});
