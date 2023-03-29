24d04e051d04ef68e52a1e68286b41bb954d5bf8
Nayru12 <ip@inesperez.net>
2023-03-30 00:01:33 +0200

    feat(final): v2.6

--- client/v2/portfolio.js
+++ client/v2/portfolio.js
@@ -60,13 +60,13 @@ const setBrands = ({result}) => {
 /**
  * Fetch products from api
  * @param  {Number}  [page=1] - current page to fetch
- * @param  {Number}  [size=12] - size of the page
+ * @param  {Number}  [size=10] - size of the page
  * @return {Object}
  */
 
 //`https://clear-fashion-api.vercel.app?page=${page}&size=${size}`
 
-const fetchProducts = async (page = 1, size = 12) => {
+const fetchProducts = async (page = 1, size = 10) => {
   try {
     const response = await fetch(
       'https://clear-fashion-nlp1.vercel.app/products?page=${page}&size=${size}'
