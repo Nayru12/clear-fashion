9f6f6bfe53f8b55c0c982148827107ecc43457b9
Nayru12 <ip@inesperez.net>
2023-04-02 11:33:36 +0200

    feat(final): v3 - api date

--- server/api.js
+++ server/api.js
@@ -22,7 +22,8 @@ app.use(helmet());
 app.options('*', cors());
 
 app.get('/', (request, response) => {
-  response.send({'ack': true});
+  response.send({'ack': true},
+  "API d'Inès PEREZ aka Nayru");
 });
 
 
